f7bfad547e94cec02ed34b08a5a5e2950c55aaf6
xin <amw_hx@163.com>
2016-08-23 10:43:16 +0800

    modify

--- task34.js
+++ task34.js
@@ -41,8 +41,8 @@ var HCIManager = function( main_controller ){
 HCIManager.prototype.Init = function(){
     this.Draw().Draw_grids();
     this.htmlElements.submit_btn.addEventListener( "click", function(){
-        this.mainController.fieldInfoManager.InterpreteAndProcess();
-        this.Draw().Draw_moving_block();
+        window.main_controller.fieldInfoManager.InterpreteAndProcess();
+        window.main_controller.hciManager.Draw().Draw_moving_block();
     } );
 }
 
@@ -82,10 +82,112 @@ var FieldInfoManager = function( main_controller ){
     this.mainController = main_controller;
     this.gridMap = null;
     this.movingBlock = null;
+    this.command = {
+        word:"",
+        step_num:0,
+        direction:0
+    };
+
 }
 
+FieldInfoManager.prototype.direction2num = function( direction ){
+    var command_direction = null;
+    switch( direction ){
+        case 'TOP':
+            command_direction = 0;
+            break;
+        case 'BOT':
+            command_direction = 2;
+            break;
+        case 'LEF':
+            command_direction = 3;
+            break;
+        case 'RIG':
+            command_direction = 1;
+            break;
+    }
+    return command_direction;
+}
+
+
+FieldInfoManager.prototype.Commands = function( arg, arg1,arg2 ){
+    var self = this;
+    return{
+        go:function(){    //translate用的也是这个函数，arg是命令，arg1是步数，arg2是方向
+            var direction = self.command.direction;
+            var step_num = self.command.step_num;
+            for( var i = 0 ; i < step_num ; i ++ ){
+                var tmp_r = self.movingBlock.rowNum;
+                var tmp_c = self.movingBlock.colNum;
+                switch( direction ){
+                    case 0:
+                        tmp_r --;
+                        break;
+                    case 1:
+                        tmp_c ++;
+                        break;
+                    case 2:
+                        tmp_r ++;
+                        break;
+                    case 3:
+                        tmp_c --;
+                        break;
+                }
+                if( tmp_r < 0 || tmp_r >= CNT_IN_ROW || tmp_c < 0 || tmp_c >= CNT_IN_COL || self.gridMap[ tmp_r ][ tmp_c ].isBlock == true ){
+                        return false;
+                }
+                self.movingBlock.rowNum = tmp_r;
+                self.movingBlock.colNum = tmp_c;
+            }
+            return true;
+        },
+        turn:function(){
+            var degree = parseInt( arg );  // 转的角度，90，-90，180
+            self.faceDirection = ( self.faceDirection + degree / 90 ) % 4;
+        },
+        turn_to:function(){
+            var direction = parseInt( arg ); // 转的朝向，0,1,2,3
+            self.faceDirection = direction;
+        }
+
+    }
+}
+
+
 FieldInfoManager.prototype.InterpreteAndProcess = function(){
-    var command = this.mainController.hciManager.htmlElements.input_command.value;
+    var com = this.mainController.hciManager.htmlElements.input_command.value.toUpperCase().split(" ");
+    if( com.length == 0 ){
+        return;
+    }
+    switch( com[0] ){
+        case 'GO':
+            this.command.word = 'GO';
+            if( com.length == 1 ){
+                this.command.step_num = 1;
+            }
+            else{
+                this.command.step_num = parseInt( com[1] );
+            }
+            this.command.direction = this.movingBlock.faceDirection;
+            this.Commands().go();
+            break;
+        case 'TRA':
+            this.command.word = 'TRA';
+            this.command.direction = this.direction2num( com[ 1 ] );
+            if( com.length == 2 ){
+                this.command.step_num = 1;
+            }
+            else{
+                this.command.step_num = parseInt( com[ 2 ] );
+            }
+            this.Commands().go();
+            break;
+        case 'TUN':
+            this.Commands().turn();
+            break;
+        case '':break;
+        case '':break;
+    }
     //interprete the command
     //manipulate the moving block
 }
@@ -101,16 +203,18 @@ FieldInfoManager.prototype.Init = function(){
     this.movingBlock = new BlockElement( CNT_IN_COL * CNT_IN_ROW, 2, 1, 0 );
 }
 
-var BlockElement = function( id, r, c){
+var BlockElement = function( id, r, c, d){
     this.id = id;
     this.rowNum = r;
     this.colNum = c;
-    this.faceDirection = null;   // 0 is north, and clockwise;
+    this.faceDirection = d;   // 0 is north, and clockwise;
+    this.isBlock = false;
 }
 
 
 window.onload = function(){
         var mainController = new MainController();
+        window.main_controller = mainController;
         var fieldManager = new FieldInfoManager( mainController );
         var hciManager = new HCIManager(mainController);
         mainController.Register( fieldManager );
